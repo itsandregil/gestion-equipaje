c338ccb8966898e76b39977eaba1fe7f1b034e40
Andre Gil <thomasgil913@gmail.com>
2024-03-19 09:07:00 -0500

    Fixed console error for reserved keyword

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,25 +4,25 @@ function NavBar() {
     return (
         <>
             
-            <nav class="bg-cyan-500 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src={takeOffLogo} class="h-12 dark:bg-white rounded-xl" alt="TakeOff Logo"/>
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Singapur Airlines</span>
+            <nav className="bg-cyan-500 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src={takeOffLogo} className="h-12 dark:bg-white rounded-xl" alt="TakeOff Logo"/>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Singapur Airlines</span>
             </a>
-            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button" class="text-black bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center">Usuario</button>
+            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button type="button" className="text-black bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center">Usuario</button>
             </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium text-xl md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-cyan-500 md:dark:bg-gray-900">
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium text-xl md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-cyan-500 md:dark:bg-gray-900">
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 md:hover:text-white md:p-0 dark:text-white dark:hover:text-cyan-600">Tu Reserva</a>
+                    <a href="#" className="block py-2 px-3 text-gray-900 md:hover:text-white md:p-0 dark:text-white dark:hover:text-cyan-600">Tu Reserva</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 md:hover:text-white md:p-0 dark:text-white dark:hover:text-cyan-600">Ofertas y Destinos</a>
+                    <a href="#" className="block py-2 px-3 text-gray-900 md:hover:text-white md:p-0 dark:text-white dark:hover:text-cyan-600">Ofertas y Destinos</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 md:hover:text-white md:p-0 dark:text-white dark:hover:text-cyan-600">Informacion y Ayuda</a>
+                    <a href="#" className="block py-2 px-3 text-gray-900 md:hover:text-white md:p-0 dark:text-white dark:hover:text-cyan-600">Informacion y Ayuda</a>
                 </li>
                 </ul>
             </div>
